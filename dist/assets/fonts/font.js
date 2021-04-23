cd84e4da538a0484b5eaefcb0025b78d9cbe9582
JonasDes <Jonas@JoBar.media>
2021-04-24 00:25:49 +0200

    te

--- dist/assets/fonts/font.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const cour_ttf_1 = __importDefault(require("./cour.ttf"));
-exports.default = cour_ttf_1.default;
-//# sourceMappingURL=font.js.map
