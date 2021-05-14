b5565ba23a1f732dc188cd9a4e2d4ccdac482151
JonasDes <Jonas@JoBar.media>
2021-05-14 17:40:50 +0200

    adds sprechw router to apihandler

--- src/handlers/ApiHandler.ts
+++ src/handlers/ApiHandler.ts
@@ -12,6 +12,7 @@ import { diveraRouter } from '../routes/divera'
 import { tentRouter } from '../routes/tent'
 import { patientRouter } from '../routes/patient'
 import { authRouter, checkAuth } from '../routes/auth'
+import { sprechWRouter } from '../routes/sprechw'
 
 const router = express.Router()
 
@@ -30,13 +31,14 @@ router.use('/vehicle', vehicleRouter)
 router.use('/vehicleType', vehicleTypeRouter)
 router.use('/userRole', userRoleRouter)
 router.use('/user', userRouter)
-router.use('/keyword', keywordRouter),
+router.use('/keyword', keywordRouter)
 router.use('/mission', missionRouter)
 router.use('/missionDiary', missionDiaryRouter)
 router.use('/operation', operationRouter)
 router.use('/divera', diveraRouter)
 router.use('/tent', tentRouter)
 router.use('/patient', patientRouter)
+router.use('/sprechw', sprechWRouter)
 
 
 export { router as apiRouter }
